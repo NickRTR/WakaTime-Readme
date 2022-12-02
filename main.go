4c9b40ef2813fdef05b2a4ce42ff62d6156507db
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-02 18:17:26 +0000

    WakaTime API Authentication with hardcoded token

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func allTime(token string) string {
+	resp, err := http.Get(fmt.Sprintf("https://wakatime.com/api/v1/users/current/all_time_since_today?api_key=%s", token))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var token string = "naaaa"
+
+	log.Println(allTime(token))
 }
