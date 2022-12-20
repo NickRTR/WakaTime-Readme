c9341075ae28e6b676257fed88c39352f3ddbf7f
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-20 19:30:05 +0000

    Debug env variables

--- main.go
+++ main.go
@@ -116,6 +116,9 @@ func createGraph(langs languages) string {
 }
 
 func main() {
+	fmt.Println(os.Environ())
+	fmt.Println(os.Args)
+
 	// environment variables for local development
 	if len(os.Args) > 1 {
 		if os.Args[1] == "test" {
